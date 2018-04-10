c0ebeecb8d2e5c66464a4b21d798eba0f7b74917
Camilo Carromeu <camilo@carromeu.com>
2018-04-10 08:48:44 -0400

    Finalização da primeira versão completa do crawler, com todos os dados sendo extraídos e o arquivo sendo salvo na Amazon S3.

--- web/index.php
+++ web/index.php
@@ -5,8 +5,43 @@ define ('URL_STK', 'http://www.fundamentus.com.br/detalhes.php?papel=');
 
 require ('../vendor/autoload.php');
 
-$tickers = require ('all.php');
+$sharedConfig = [
+    'region'  => 'sa-east-1',
+    'version' => 'latest'
+];
 
-$stocks = require ('stocks.php');
+$sdk = new Aws\Sdk ($sharedConfig);
 
-echo json_encode ($stocks);
+$client = $sdk->createS3 ();
+
+$bucket = 'stock-score';
+
+$file = 'fundamentus-'. date ('Ymd');
+
+$exists = $client->doesObjectExist ($bucket, $file);
+
+if ($exists)
+{
+    $result = $client->getObject([
+        'Bucket' => 'stock-score',
+        'Key'    => $file
+    ]);
+
+    $json = $result ['Body'];
+}
+else
+{
+    $tickers = require ('all.php');
+
+    $stocks = require ('stocks.php');
+
+    $json = json_encode ($stocks);
+
+    $result = $client->putObject([
+        'Bucket' => $bucket,
+        'Key'    => $file,
+        'Body'   => $json
+    ]);
+}
+
+echo $json;
--- web/stocks.php
+++ web/stocks.php
@@ -3,17 +3,17 @@
 unset ($html);
 unset ($out);
 
+// Debug
+// $tickers = array ('ABEV3');
+
 $stocks = array ();
 
 foreach ($tickers as $trash => $ticker)
 {
     $html = utf8_encode (file_get_contents (URL_STK . $ticker));
 
-    // die (strip_tags ($html));
-
-    // <span class="help tips" title="Código da ação">?</span><span class="txt">Papel</span>
-
     // Getting labels of attributes and its description...
+    // <span class="help tips" title="Código da ação">?</span><span class="txt">Papel</span>
     /*
     preg_match_all ('/<span class="help tips" title="([^"]+)">\?<\/span><span class="txt">([^<]+)<\/span>/', $html, $out);
     
@@ -24,16 +24,43 @@ foreach ($tickers as $trash => $ticker)
     
     echo json_encode ($tips);
     */
-
-    // <td class="label"><span class="help tips" title="Data do último pregão em  que o ativo foi negociado">?</span><span class="txt">Data últ cot</span></td>
-    // <td class="data"><span class="txt">06/04/2018</span></td>
     
-    preg_match_all ('/<td class="label"><span class="help tips" title="[^"]+">\?<\/span><span class="txt">([^<]+)<\/span><\/td>[\s]+<td class="data"><span class="txt">([^<]+)<\/span><\/td>/', $html, $out);
+    $html = str_replace (array ("\r", "\n", "\t"), '', $html);
+
+    $html = preg_replace ('/(\>)\s*(\<)/m', '$1$2', $html);
+
+    preg_match_all ('#</span><span class="txt">([^<]+)</span></td><td class="data[^"]*"><span class="txt">([^<]+)</span>#u', $html, $out1);
+
+    preg_match_all ('#</span><span class="txt">([^<]+)</span></td><td class="data[^"]*"><span class="txt"><a href="[^"]+">([^<]+)</a></span>#u', $html, $out2);
+
+    preg_match_all ('#<span class="txt">([^<]+)</span></td><td class="data[^"]*"><span class="oscil"><font color="[^"]+">([^<]+)</font></span>#u', $html, $out3);
+
+    // Debug
+    /*
+    print_r ($out1);
+    print_r ($out2);
+    print_r ($out3);
+    die ();
+    */
 
     $attributes = array ('ticker' => $ticker);
     
-    foreach ($out [0] as $key => $trash)
-        $attributes [$out [1][$key]] = trim ($out [2][$key]);
+
+    foreach ($out1 [0] as $key => $trash)
+        if (!in_array ($out1 [1][$key], array ('Receita Líquida', 'EBIT', 'Lucro Líquido')))
+            $attributes [$out1 [1][$key]] = trim (strip_tags ($out1 [2][$key]));
+        else
+        {
+            $aux = array_key_exists ($out1 [1][$key] .' 12 meses', $attributes) ? $out1 [1][$key] .' 3 meses' : $out1 [1][$key] .' 12 meses';
+
+            $attributes [$aux] = trim (strip_tags ($out1 [2][$key]));
+        }
+    
+    foreach ($out2 [0] as $key => $trash)
+        $attributes [$out2 [1][$key]] = trim (strip_tags ($out2 [2][$key]));
+    
+    foreach ($out3 [0] as $key => $trash)
+        $attributes ['Oscilações - '. $out3 [1][$key]] = trim (strip_tags ($out3 [2][$key]));
     
     $stocks [] = (Object) $attributes;
 }
